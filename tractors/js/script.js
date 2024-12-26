49e4d4633e774bdfb10be9195ab6e301cbe86327
passkuda <122729903+passkuda@users.noreply.github.com>
2024-12-26 20:20:40 +0200

    Add files via upload

--- tractors/js/script.js
+++ tractors/js/script.js
@@ -14,3 +14,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	// Navigation arrows
+	navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+	},
+});
